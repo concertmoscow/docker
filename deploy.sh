2a138c93b5ce0c2caf6176c7debdd6008bafdd88
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-05-26 00:04:07 +0300

    docker-compose down && docker-compose up

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ rm -rf ./back
 rm -rf ./front
 git clone https://github.com/concertmoscow/back
 git clone https://github.com/concertmoscow/front
-docker-compose up
+docker-compose down && docker-compose up
