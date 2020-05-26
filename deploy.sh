e5908c7a4b17af6d2334516312f7ef336e74e96a
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-05-26 22:07:24 +0300

    +rebuild

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ rm -rf ./back
 rm -rf ./front
 git clone https://github.com/concertmoscow/back
 git clone https://github.com/concertmoscow/front
-#docker-compose build
+docker-compose build
 docker-compose up -d
