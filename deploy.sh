8c2b2cf3c623f6df9f90eacc3a28e7b8ac62c439
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-05-27 13:56:07 +0300

    rm -rf ./data mv ./_data ./data

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 docker-compose down
 rm -rf ./back
 rm -rf ./front
+mv ./data ./_data
 git clone https://github.com/concertmoscow/back
 git clone https://github.com/concertmoscow/front
 docker-compose build
 docker-compose up -d
+rm -rf ./data
+mv ./_data ./data
